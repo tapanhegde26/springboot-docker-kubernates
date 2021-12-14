bb7793cddf49b82ebddab0d7912ee7c2db13e662
c5281159 <tapan.kumar.hegde@sap.com>
2021-12-14 18:39:08 +0530

    initial commit

---
+++ springboot-kubernates/src/main/java/com/learn/tapan/springbootkubernates/SpringbootKubernatesApplication.java
@@ -0,0 +1,13 @@
+package com.learn.tapan.springbootkubernates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKubernatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKubernatesApplication.class, args);
+	}
+
+}
