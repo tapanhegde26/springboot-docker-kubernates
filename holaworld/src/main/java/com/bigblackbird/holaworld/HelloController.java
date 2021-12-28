baac9269f684f841f632cfc41397075f99aa2dc8
sheldon-cooper26 <55941193+sheldon-cooper26@users.noreply.github.com>
2021-12-28 17:03:13 +0530

    Update HelloController.java

--- holaworld/src/main/java/com/bigblackbird/holaworld/HelloController.java
+++ holaworld/src/main/java/com/bigblackbird/holaworld/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/greet")
     public String index() {
-        return "tan tana tan tan tan tara...chalti he kya nou do gyara!";
+        return "Why do we fall bruce!!?.. Just to rise up again!!";
     }
 }
